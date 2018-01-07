cfa6a317bef72576551a28b6d111b337a287c177
jsulca <joelsulca.c@gmail.com>
2018-01-07 15:03:14 -0500

    plantilla para adm seguridad

---
+++ src/app/app.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
+import * as hopscotch from 'hopscotch';
+
+import { RoutePartsService } from "./services/route-parts/route-parts.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  appTitle = 'Egret';
+  pageTitle = '';
+
+  constructor(public title: Title, 
+    private router: Router, 
+    private activeRoute: ActivatedRoute,
+    private routePartsService: RoutePartsService,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.changePageTitle();
+    // Init User Tour
+    setTimeout(() => {
+      hopscotch.startTour(this.tourSteps());
+    }, 2000);
+  }
+  /*
+  ***** Tour Steps ****
+  * You can supply tourSteps directly in hopscotch.startTour instead of 
+  * returning value by invoking tourSteps method,
+  * but DOM query methods(querySelector, getElementsByTagName etc) will not work
+  */
+  tourSteps(): any {
+    let self = this;
+    return {
+      id: 'hello-egret',
+      showPrevButton: true,
+      onEnd: function() {
+        self.snackBar.open('Awesome! Now let\'s explore Egret\'s cool features.', 'close', { duration: 5000 });
+      },
+      onClose: function() {
+        self.snackBar.open('You just closed User Tour!', 'close', { duration: 3000 });
+      },
+      steps: [
+        {
+          title: 'Sidebar Controls',
+          content: 'Control left sidebar\'s display style.',
+          target: 'sidenavToggle', // Element ID
+          placement: 'bottom',
+          xOffset: 10
+        },
+        {
+          title: 'Available Themes',
+          content: 'Choose a color scheme.',
+          target: 'schemeToggle', // Element ID
+          placement: 'left',
+          xOffset: 20
+        },
+        {
+          title: 'Language',
+          content: 'Choose your language.',
+          target: document.querySelector('.topbar .mat-select'),
+          placement: 'left',
+          xOffset: 10,
+          yOffset: -5
+        }
+      ]
+    }
+  }
+  changePageTitle() {
+    this.router.events.filter(event => event instanceof NavigationEnd).subscribe((routeChange) => {
+      var routeParts = this.routePartsService.generateRouteParts(this.activeRoute.snapshot);
+      if (!routeParts.length)
+        return this.title.setTitle(this.appTitle);
+      // Extract title from parts;
+      this.pageTitle = routeParts
+                      .reverse()
+                      .map((part) => part.title )
+                      .reduce((partA, partI) => {return `${partA} > ${partI}`});
+      this.pageTitle += ` | ${this.appTitle}`;
+      this.title.setTitle(this.pageTitle);
+      
+    });
+  }
+  
+}
---
+++ src/app/services/navigation/navigation.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+interface IMenuItem {
+  type: string,       // Possible values: link/dropDown/icon/separator/extLink
+  name?: string,      // Used as display text for item and title for separator type
+  state?: string,     // Router state
+  icon?: string,      // Item icon name
+  tooltip?: string,   // Tooltip text
+  disabled?: boolean, // If true, item will not be appeared in sidenav.
+  sub?: IChildItem[]  // Dropdown items
+}
+interface IChildItem {
+  name: string,       // Display text
+  state: string       // Router state
+}
+
+@Injectable()
+export class NavigationService {
+  constructor() {}
+
+  defaultMenu:IMenuItem[] = [
+    {
+      name: 'PANEL PRINCIPAL',
+      type: 'link',
+      tooltip: 'Panel principal',
+      icon: 'home',
+      state: 'dashboard'
+    },
+    {
+      name: 'GESTION DE USUARIOS',
+      type: 'link',
+      tooltip: 'Gestión de usuarios',
+      icon: 'face',
+      state: 'dashboard'
+    }
+  ]
+
+
+  // Icon menu TITLE at the very top of navigation.
+  // This title will appear if any icon type item is present in menu.
+  iconTypeMenuTitle:string = 'Frequently Accessed';
+  // sets iconMenu as default;
+  menuItems = new BehaviorSubject<IMenuItem[]>(this.defaultMenu);
+  // navigation component has subscribed to this Observable
+  menuItems$ = this.menuItems.asObservable();
+}
